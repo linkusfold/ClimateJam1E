ee0bb7e748859a1df106250dca01521f575cdbbd
Vincent-207 <vhannah2007@outlook.com>
2025-06-27 13:06:09 -0500

    Created Singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+
+    
+}
