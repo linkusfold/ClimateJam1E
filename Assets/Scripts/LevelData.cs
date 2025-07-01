f5e0170e315d0b93a862962e9212b949dc0067c3
TheDarkLord05 <156219520+FlameMan05@users.noreply.github.com>
2025-07-01 17:07:06 -0400

    Update LevelData.cs

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewLevelData", menuName = "ScriptableObjects/LevelData", order = 2)]
 public class LevelData : ScriptableObject
 {
+    public float countdown = 5f;
+    public int maxEnemiesSafe = 4;
     public WaveData[] waves;
 }
