755db5b59060350f20388dd735dbf7edd980a372
TheDarkLord05 <156219520+FlameMan05@users.noreply.github.com>
2025-07-01 16:31:34 -0400

    Added LevelData Script

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevelData", menuName = "ScriptableObjects/LevelData", order = 2)]
+public class LevelData : ScriptableObject
+{
+    public WaveData[] waves;
+}
