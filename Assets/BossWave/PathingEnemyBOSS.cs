2e260f32ebfe5cdfd0f912d8cea50b10bcf206ca
Declan Smith <ddds2005@gmail.com>
2025-07-02 21:01:12 -0400

    fixes to help merge

---
+++ Assets/BossWave/PathingEnemyBOSS.cs
@@ -0,0 +1,76 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    // This is an abstract class for the minions that disasters can spawn. 
+    // They follow a path and deal damage if they get to the end.
+    // They also each have a unique attack.
+    public abstract class PathingEnemyBOSS : Enemy
+    {
+        public Path path;
+        public int currentNodeId = 1;
+        public bool pathing = true;
+
+
+        protected override void Start()
+        {
+            base.Start();
+
+            if (path == null)
+            {
+                Debug.LogError("Enemy " + gameObject.name + " has no path!");
+                return;
+            }
+            TeleportToPathNode(currentNodeId);
+            GoToPathNode(currentNodeId + 1);
+        }
+
+        protected override void FixedUpdate()
+        {
+            if (!pathing) return;
+            if (transform.position == path.pathNodes[currentNodeId])
+            {
+                ReachedNode();
+                return;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, path.pathNodes[currentNodeId], speed * Time.deltaTime);
+        }
+
+        protected void GoToPathNode(int nodeId)
+        {
+            currentNodeId = nodeId;
+            pathing = true;
+        }
+
+        protected void ReachedNode()
+        {
+            pathing = false;
+            Debug.Log("Enemy " + gameObject.name + " reached Node " + currentNodeId + "!");
+
+            if (path.pathNodes.Count <= currentNodeId + 1)
+            {
+                OnReachedEnd();
+                return;
+            }
+
+            currentNodeId++;
+            GoToPathNode(currentNodeId);
+            TakeDamage(50f); //Debug testing
+        }
+        protected void OnReachedEnd()
+        {
+            waveSpawner.EnemiesSafe++;
+            waveSpawner.waves[waveSpawner.currentWaveIndex].enemiesLeft--;
+            //Once we reach the end of the path we deal damage to the town/house
+            Debug.Log($"{gameObject.name} reached the end and dealt {damage} damage!");
+            Destroy(gameObject); // Remove the enemy
+        }
+
+        public void TeleportToPathNode(int nodeId)
+        {
+            Vector3 nodePos = path.pathNodes[nodeId];
+            transform.position = nodePos;
+        }
+    }
+}
---
+++ Assets/BossWave/WaveSpawnerBOSS.cs
@@ -0,0 +1,109 @@
+using DefaultNamespace;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class WaveSpawnerBOSS : MonoBehaviour
+{
+    [SerializeField] private float countdown;
+
+    [SerializeField] private GameObject spawnPoint;
+
+    public Wave[] waves;
+
+    public int currentWaveIndex = 0;
+
+    private bool readyToCountDown;
+
+    public int EnemiesSafe = 0;
+
+    private void Start()
+    {
+        Restart();
+    }
+
+    public void Restart()
+    {
+        currentWaveIndex = 0;
+        EnemiesSafe = 0;
+
+        readyToCountDown = true;
+
+        for (int i = 0; i < waves.Length; i++)
+        {
+            waves[i].enemiesLeft = waves[i].enemies.Length;
+        }
+    }
+
+    private void Update()
+    {
+        //if (EnemiesSafe >= 4)
+        //{
+        //    Application.Quit();
+        //}
+
+        if (currentWaveIndex >= waves.Length)
+        {
+            Debug.Log("You survived every wave!");
+            return;
+        }
+
+        if (readyToCountDown == true)
+        {
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdown <= 0)
+        {
+            readyToCountDown = false;
+            countdown = waves[currentWaveIndex].timeToNextWave;
+            StartCoroutine(SpawnWave());
+
+            Debug.Log("Starting wave " + currentWaveIndex);
+        }
+        if (waves[currentWaveIndex].enemiesLeft == 0)
+        {
+            readyToCountDown = true;
+            currentWaveIndex++;
+        }
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        if (currentWaveIndex < waves.Length)
+        {
+            for (int i = 0; i < waves[currentWaveIndex].enemies.Length; i++)
+            {
+                Enemy enemyPrefab = waves[currentWaveIndex].enemies[i];
+
+                if (enemyPrefab is PathingEnemy pathingEnemyPrefab)
+                {
+                    pathingEnemyPrefab.path = waves[currentWaveIndex].path;
+
+                    PathingEnemy pathingEnemy = Instantiate(pathingEnemyPrefab, spawnPoint.transform);
+                    pathingEnemy.currentNodeId = 1;
+                    pathingEnemy.transform.SetParent(spawnPoint.transform);
+                }
+                else
+                {
+                    Enemy enemy = Instantiate(enemyPrefab, spawnPoint.transform);
+                    enemy.transform.SetParent(spawnPoint.transform);
+                }
+
+                yield return new WaitForSeconds(waves[currentWaveIndex].timeToNextEnemy);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class WaveBOSS
+{
+    public Enemy[] enemies;
+    public Path path;
+    public float timeToNextEnemy;
+    public float timeToNextWave;
+
+    [HideInInspector] public int enemiesLeft;
+}
