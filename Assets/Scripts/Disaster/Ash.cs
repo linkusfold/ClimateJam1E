443fdc0cc5d234e345c17c81fd9d86fdfdd76ba6
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2025-07-20 16:47:55 -0700

    Falling ash attack

---
+++ Assets/Scripts/Disaster/Ash.cs
@@ -0,0 +1,22 @@
+using DefaultNamespace;
+using UnityEngine;
+using System.Collections;
+    public class Ash : Enemy
+    // Falling ash attack
+    {   
+        protected override void Start()
+        {
+            speed = 0.5f;
+            health = 50;
+            damage = 2;
+            atkSpeed = 1; //only attacks once
+            base.Start();
+        }
+
+        protected override void PerformAttack(IDamageableBuilding building)
+    {
+        building.TakeDamage((int)damage);
+        //Die();
+    }
+
+    }
